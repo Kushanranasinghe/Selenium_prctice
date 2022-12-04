31119a2ef30dbda31a09f80a77b3f2b2ca7e7c8a
Kushanranasinghe <Kushanr91@yahoo.com>
2022-12-04 22:38:47 +0530

    Initial commit

---
+++ src/test/java/Locators3.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators3 {
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kushan Ranasinghe\\Desktop\\Chrome\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        //Siblings -child to Parent traverse
+        // Sibling - Child to parent traverse
+
+//header/div/button[1]/following-sibling::button[1]
+
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+
+        System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+
+     //   driver.manage().window().maximize();
+
+      //  driver.get("http://google.com");
+
+      //  driver.navigate().to("https://rahulshettyacademy.com");
+
+     //   driver.navigate().back();
+
+     //   driver.navigate().forward();
+
+    }
+
+    }
+
---
+++ src/test/java/loca1.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class loca1 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Kushan Ranasinghe\\Desktop\\projects\\chrome\\chromedriver_win32\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        //Sibling - child to parent travers
+
+
+    }
+}
